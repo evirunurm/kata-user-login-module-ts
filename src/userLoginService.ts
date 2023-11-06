e94880f83d9954f636533953c7d7dd405e359e5f
Denis Karadaliev <deniskaradaliev@gmail.com>
2023-11-06 16:30:52 +0100

    paso 2 hecho

--- src/userLoginService.ts
+++ src/userLoginService.ts
@@ -3,10 +3,19 @@ import {User} from "./user"
 export class UserLoginService {
     private loggedUsers: User[] = []
 
-    public manualLogin = (): string => {
-        return "user logged"
+    public manualLogin = (user: User): string => {
+        if (this.isUserAlreadyLogged(user)) {
+            return "User already logged in"
+        }
+
+        this.loggedUsers.push(user)
+        return "User successfully logged in"
+    }
+
+    getLoggedUsers(){
+        return this.loggedUsers.map(user=>user.getUserName())
     }
 
 
-    private isUserAlreadyLogged = (user: User)=> this.loggedUsers.some(loggedUser => loggedUser.getUserName() === user.getUserName())
+    private isUserAlreadyLogged = (user: User) => this.loggedUsers.some(loggedUser => loggedUser.getUserName() === user.getUserName())
 }
--- test/userLogin.test.ts
+++ test/userLogin.test.ts
@@ -1,11 +1,38 @@
 import {UserLoginService} from "../src/userLoginService";
+import {User} from "../src/user";
 
 
 describe('User Service Login', () => {
-    const service = new UserLoginService()
 
     it('should log a user', () => {
-        expect(service.manualLogin()).toEqual('user logged')
+        const service = new UserLoginService()
+        const user = new User("Javier")
+
+        const response = service.manualLogin(user)
+
+        expect(response).toEqual('User successfully logged in')
+    })
+
+    it('should not log an already logged user', () => {
+        const service = new UserLoginService()
+        const user = new User("Javier")
+        service.manualLogin(user)
+
+        const response = service.manualLogin(user)
+
+        expect(response).toEqual('User already logged in')
+    })
+
+    it('should return us a logged users array',()=>{
+        const service = new UserLoginService()
+        const user = new User("Javier")
+        const user2= new User("Denis")
+        service.manualLogin(user)
+        service.manualLogin(user2)
+
+        const users=service.getLoggedUsers()
+
+        expect(users).toEqual(["Javier","Denis"])
     })
 
 })
